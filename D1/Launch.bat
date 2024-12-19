dc481cdf8a632af267c5c30452521df98167942c
unknown <bji0519@gmail.com>
2024-12-19 22:51:24 +0900

    Chore: 에셋 매니저 설정

---
+++ D1/Launch.bat
@@ -0,0 +1,2 @@
+call "C:/Program Files/Epic Games/UE_5.4/Engine/Build/BatchFiles/Build.bat" ^
+-projectfiles -project="C:/Users/bji15/source/repos/D1-Live/D1/D1.uproject" -game -rocket -progress -log="C:\Users\bji15\source\repos\D1-Live\D1/Saved/Logs/UnrealVersionSelector-2024.12.19-22.44.41.log"
