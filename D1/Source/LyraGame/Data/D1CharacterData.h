ac4ba6cd30a005938f0e0e9e1f539223efb0c4a7
unknown <bji0519@gmail.com>
2024-12-21 00:57:02 +0900

    Chore: Character Data Setting

--- D1/Source/LyraGame/Data/D1CharacterData.h
+++ D1/Source/LyraGame/Data/D1CharacterData.h
@@ -2,9 +2,37 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "D1Define.h"
 #include "Engine/DataAsset.h"
 #include "D1CharacterData.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FD1DefaultArmorMeshSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UMaterialInterface> UpperBodySkinMaterial;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UMaterialInterface> LowerBodySkinMaterial;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<USkeletalMesh> HeadDefaultMesh;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<USkeletalMesh> HeadSecondaryMesh;
+
+public:
+	UPROPERTY(EditDefaultsOnly, meta=(ArraySizeEnum="EArmorType"))
+	TSoftObjectPtr<USkeletalMesh> DefaultMeshEntries[(int32)EArmorType::Count];
+
+	UPROPERTY(EditDefaultsOnly, meta = (ArraySizeEnum = "EArmorType"))
+	TSoftObjectPtr<USkeletalMesh> SecondaryMeshEntries[(int32)EArmorType::Count];
+};
 
 /**
  * 
@@ -13,5 +41,19 @@ UCLASS()
 class LYRAGAME_API UD1CharacterData : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	static const UD1CharacterData& Get();
+
+public:
+#if WITH_EDITOR
+	virtual void PreSave(FObjectPreSaveContext ObjectSaveContext) override;
+#endif	// WITH_EDITOR
+
+public:
+	const FD1DefaultArmorMeshSet& GetDefaultArmorMeshSet(ECharacterSkinType SkinType) const;
 	
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TMap<ECharacterSkinType, FD1DefaultArmorMeshSet> DefaultArmorMeshMap;
 };
